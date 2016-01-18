33d8165b98a5c965f2a0fbae480996a4d5e798d7
XBindt <xbindt@mirabeau.nl>
2016-01-18 15:17:53 +0100

    script aangepast naar nieuwe json parser

--- nsapi.js
+++ nsapi.js
@@ -1,25 +1,25 @@
 /*
-Name:       nsapi.js - Unofficial NodeJS module for Nederlandse Spoorwegen API
-Author:     Franklin van de Meent
-Source:     https://github.com/fvdm/nodejs-ns-api
-Feedback:   https://github.com/fvdm/nodejs-ns-api/issues
-API Docs:   http://www.ns.nl/api/api
-License:    Unlicense / Public Domain
-            (see UNLICENSE file or https://raw.github.com/fvdm/nodejs-ns-api/master/UNLICENSE)
-*/
+ Name:       nsapi.js - Unofficial NodeJS module for Nederlandse Spoorwegen API
+ Author:     Franklin van de Meent
+ Source:     https://github.com/fvdm/nodejs-ns-api
+ Feedback:   https://github.com/fvdm/nodejs-ns-api/issues
+ API Docs:   http://www.ns.nl/api/api
+ License:    Unlicense / Public Domain
+ (see UNLICENSE file or https://raw.github.com/fvdm/nodejs-ns-api/master/UNLICENSE)
+ */
 
 var http = require('http'),
-    util = require('util'),
-    querystring = require('querystring'),
-    xml2json = require('xml2json'),
-    app = {username: '', password: ''}
+		util = require('util'),
+		querystring = require('querystring'),
+		parseString = require("xml2js").parseString,
+		app = {username: '', password: ''}
 
 
 // ! Vertrektijden
 app.vertrektijden = function( station, callback ) {
 	app.talk( 'avt', {station: station}, function( err, data ) {
 		if( !err ) {
-			if( !data.ActueleVertrekTijden || !data.ActueleVertrekTijden.VertrekkendeTrein ) {
+			if( !data.actueleVertrekTijden || !data.actueleVertrekTijden.VertrekkendeTrein ) {
 				callback( new Error('unexpected response') )
 			} else {
 				data = data.ActueleVertrekTijden.VertrekkendeTrein
@@ -51,23 +51,24 @@ app.prijzen = function( props, callback ) {
 // ! Reisadviezen
 app.reisadvies = function( props, callback ) {
 	app.talk( 'treinplanner', props, function( err, data ) {
+
 		if( !err ) {
 			if( !data.ReisMogelijkheden || !data.ReisMogelijkheden.ReisMogelijkheid ) {
 				callback( new Error('unexpected response') )
 			} else {
 				data = data.ReisMogelijkheden.ReisMogelijkheid
-				
+
 				if( !util.isArray( data ) ) {
 					data = [data]
 				}
-				
+
 				if( data.length >= 1 ) {
 					for( var r in data ) {
 						var reis = data[r]
 						if( !util.isArray( reis.ReisDeel ) ) {
 							reis.ReisDeel = [reis.ReisDeel]
 						}
-						
+
 						for( var d in reis.ReisDeel ) {
 							var deel = reis.ReisDeel[d]
 							for( var s in deel.ReisStop ) {
@@ -81,11 +82,11 @@ app.reisadvies = function( props, callback ) {
 							}
 							reis.ReisDeel[d] = deel
 						}
-						
+
 						data[r] = reis
 					}
 				}
-				
+
 				callback( null, data )
 			}
 		} else {
@@ -101,17 +102,19 @@ app.stations = function( treeKey, callback ) {
 		var callback = treeKey
 		var treeKey = 'code'
 	}
-	
+
 	app.talk( 'stations-v2', function( err, data ) {
+
 		if( !err ) {
 			if( !data.Stations.Station ) {
 				callback( new Error('unexpected response') )
 			} else {
-				data = data.Stations.Station
+				data = data.Stations.Station;
 				var tree = {}
 				for( var s in data ) {
 					var station = data[s]
-					
+
+
 					if( treeKey === 'code' ) {
 						tree[ station.Code ] = station
 					} else if( typeof station[ treeKey ] !== 'string' ) {
@@ -124,7 +127,7 @@ app.stations = function( treeKey, callback ) {
 						tree[ station[ treeKey ] ][ station.Code ] = station
 					}
 				}
-				
+
 				callback( null, tree )
 			}
 		} else {
@@ -144,15 +147,15 @@ app.storingen = function( params, callback ) {
 				data = data.Storingen
 				data.Ongepland = data.Ongepland.Storing || []
 				data.Gepland = data.Gepland.Storing || []
-				
+
 				if( !util.isArray( data.Ongepland ) ) {
 					data.Ongepland = [data.Ongepland]
 				}
-				
+
 				if( !util.isArray( data.Gepland ) ) {
 					data.Gepland = [data.Gepland]
 				}
-				
+
 				callback( null, data )
 			}
 		} else {
@@ -168,17 +171,17 @@ app.talk = function( path, props, callback ) {
 		var callback = props
 		var props = {}
 	}
-	
+
 	var options = {
 		host:	'webservices.ns.nl',
-		port:	80,
+		port: 80,
 		path:	'/ns-api-'+ path +'?'+ querystring.stringify( props ),
 		method:	'GET',
 		auth:	app.username +':'+ app.password
 	}
-	
+
 	var req = http.request( options )
-	
+
 	req.on( 'response', function( response ) {
 		var data = ''
 		response.on( 'data', function( ch ) { data += ch })
@@ -193,15 +196,14 @@ app.talk = function( path, props, callback ) {
 						message:	error
 					}
 					callback( err )
+
 				})
 			} else if( data.match('<?xml') ) {
-				data = xml2json.toJson( data, {
-					object: true,
-					coerce: true,
-					trim: true,
-					sanitize: false
-				})
-				callback( null, data )
+				parseString(data, {trim: true, emptyTag:{}, normalize: true}, function (err, result) {
+					callback( null, result )
+
+				});
+
 			} else {
 				var err = new Error('invalid response')
 				err.details = {
@@ -215,13 +217,13 @@ app.talk = function( path, props, callback ) {
 			}
 		})
 	})
-	
+
 	req.on( 'error', function( error ) {
 		var err = new Error('request failed')
 		err.details = error
 		callback( err )
 	})
-	
+
 	req.end()
 }
 
